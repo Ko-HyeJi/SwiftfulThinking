214d66cf9cb2dec4010e67327c775928bb10c055
고혜지 <kohyeji@gohyejiui-MacBookPro.local>
2023-06-27 00:42:28 +0900

    Text

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/TextBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  TextBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by 고혜지 on 2023/06/14.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text("Hello, World! This is the Swiftful Thinking Bootcamp. I am really enjoying this course and learning alo.".capitalized)
+//            .font(.body)
+//            .fontWeight(.semibold)
+//            .bold()
+//            .underline()
+//            .underline(true, color: Color.red)
+//            .italic()
+//            .strikethrough(true, color: Color.green)
+//            .font(.system(size: 24, weight: .semibold, design: .serif))
+//            .baselineOffset(-50.0)
+//            .kerning(10)
+//            .multilineTextAlignment(.leading)
+//            .foregroundColor(.red)
+//            .frame(width: 200, height: 100, alignment: .leading)
+//            .minimumScaleFactor(0.1)
+    }
+}
+
+struct TextBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextBootcamp()
+    }
+}
