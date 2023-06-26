9a4286cce4effd137b6f514817ede5765d8ec0a5
고혜지 <kohyeji@gohyejiui-MacBookPro.local>
2023-06-27 00:46:09 +0900

    BackgroundAndOverlay

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/BackgroundAndOverlayBootcamp.swift
@@ -0,0 +1,86 @@
+//
+//  BackgroundAndOverlayBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by 고혜지 on 2023/06/15.
+//
+
+import SwiftUI
+
+struct BackgroundAndOverlayBootcamp: View {
+    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .background(
+//                Circle()
+//                    .fill(LinearGradient(colors: [.red, .blue], startPoint: .leading, endPoint: .trailing))
+//                    .frame(width: 100, height: 100, alignment: .center)
+//            )
+//            .background(
+//                Circle()
+//                    .fill(LinearGradient(colors: [.blue, .red], startPoint: .leading, endPoint: .trailing))
+//                    .frame(width: 120, height: 120, alignment: .center)
+//            )
+        
+//        Circle()
+//            .fill(.pink)
+//            .frame(width: 100, height: 100, alignment: .center)
+//            .overlay(
+//                Text("1")
+//                    .font(.largeTitle)
+//                    .foregroundColor(.white)
+//            )
+//            .background(
+//                Circle()
+//                    .fill(.purple)
+//                    .frame(width: 110, height: 110, alignment: .center)
+//            )
+        
+//        Rectangle()
+//            .frame(width: 100, height: 100)
+//            .overlay(
+//                Rectangle()
+//                    .fill(.blue)
+//                    .frame(width: 50, height: 50)
+//                , alignment: .topLeading
+//            )
+//            .background(
+//                Rectangle()
+//                    .fill(.red)
+//                    .frame(width: 150, height: 150)
+//                , alignment: .bottomTrailing
+//            )
+        
+        Image(systemName: "heart.fill")
+            .font(.system(size: 40))
+            .foregroundColor(.white)
+            .background(
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [.red, .blue],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing)
+                    )
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 10)
+                    .overlay(
+                        Circle()
+                            .fill(.blue)
+                            .frame(width: 35, height: 35)
+                            .overlay(
+                                Text("5")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                            )
+                            .shadow(color: .gray.opacity(0.5), radius: 10, x: 5, y: 5)
+                        , alignment: .bottomTrailing
+                    )
+            )
+    }
+}
+
+struct BackgroundAndOverlayBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundAndOverlayBootcamp()
+    }
+}
