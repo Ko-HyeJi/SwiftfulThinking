a7f8c159f637c67fc1babb52d92c9bdb2a0a37ed
고혜지 <kohyeji@gohyejiui-MacBookPro.local>
2023-06-27 00:52:41 +0900

    Create Project

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/SwiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by 고혜지 on 2023/06/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
