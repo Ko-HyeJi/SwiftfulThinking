9d17b9c85c2e81c928bd2679788d70d3259d14f3
고혜지 <kohyeji@gohyejiui-MacBookPro.local>
2023-06-28 00:42:06 +0900

    State

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ExtractedFunctionsBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ExtractedFunctionsBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by 고혜지 on 2023/06/28.
+//
+
+import SwiftUI
+
+struct ExtractedFunctionsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExtractedFunctionsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ExtractedFunctionsBootcamp()
+    }
+}
