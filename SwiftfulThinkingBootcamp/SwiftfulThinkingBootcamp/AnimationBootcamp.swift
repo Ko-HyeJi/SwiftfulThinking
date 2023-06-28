8309f7c35c0aad61da1cce0de7dde3c6609597c1
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-06-28 22:05:56 +0900

    Animation

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/AnimationBootcamp.swift
@@ -0,0 +1,44 @@
+//
+//  AnimationBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by 고혜지 on 2023/06/28.
+//
+
+import SwiftUI
+
+struct AnimationBootcamp: View {
+    
+    @State var isAnimated: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button("Button") {
+                withAnimation(
+                    Animation
+                        .default
+//                        .delay(2.0)
+//                        .repeatCount(5, autoreverses: true)
+                        .repeatForever(autoreverses: true)
+                ){
+                    isAnimated.toggle()
+                }
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25)
+                .fill(isAnimated ? Color.red : Color.green)
+                .frame(
+                    width: isAnimated ? 100 : 300,
+                    height: isAnimated ? 100 : 300)
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0))
+                .offset(y: isAnimated ? 300 : 0)
+            Spacer()
+        }
+    }
+}
+
+struct AnimationBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationBootcamp()
+    }
+}
