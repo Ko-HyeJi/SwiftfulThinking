ad15e731c16503cc93b8334193621257414e8a35
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-07-19 23:12:16 +0900

    EnvironmentObject

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/EnvironmentObjectBootcamp.swift
@@ -0,0 +1,106 @@
+//
+//  EnvironmentObjectBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by 고혜지 on 2023/07/19.
+//
+
+import SwiftUI
+
+// ObservedObject
+// StateObject
+// EnvironmentObject
+
+class EnvironmentViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    init() {
+        getData()
+    }
+    
+    func getData() {
+        self.dataArray.append(contentsOf: ["iPhone", "iPad", "iMac", "Apple Watch"])
+    }
+}
+
+struct EnvironmentObjectBootcamp: View {
+    
+    @StateObject var viewModel: EnvironmentViewModel = EnvironmentViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.dataArray, id: \.self) { item in
+                    NavigationLink {
+                        DetailView(selectedItem: item)
+                    } label: {
+                        Text(item)
+                    }
+                }
+            }
+            .navigationTitle("iOS Devices")
+        }
+        .environmentObject(viewModel)
+    }
+}
+
+struct DetailView: View {
+    
+    let selectedItem: String
+    
+    var body: some View {
+        ZStack {
+            // background
+            Color.orange.ignoresSafeArea()
+            
+            // foreground
+            NavigationLink {
+                FinalView()
+            } label: {
+                Text(selectedItem)
+                    .font(.headline)
+                    .foregroundColor(.orange)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(.white)
+                    .cornerRadius(30)
+            }
+        }
+    }
+}
+
+struct FinalView: View {
+    
+    @EnvironmentObject var viewModel: EnvironmentViewModel
+    
+    var body: some View {
+        ZStack {
+            // background
+            LinearGradient(
+                gradient: Gradient(colors: [Color.cyan, Color.blue]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+            
+            // foreground
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(viewModel.dataArray, id: \.self) { item in
+                        Text(item)
+                    }
+                }
+                .foregroundColor(.white)
+                .font(.largeTitle)
+            }
+        }
+    }
+}
+
+struct EnvironmentObjectBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        EnvironmentObjectBootcamp()
+//        DetailView(selectedItem: "iPhone")
+//        FinalView()
+    }
+}
