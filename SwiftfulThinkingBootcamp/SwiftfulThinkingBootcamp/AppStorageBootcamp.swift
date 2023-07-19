57ac9b9190594c756ccd7aa43a2bcc47f990cfb9
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-07-19 23:28:54 +0900

    AppStorage

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/AppStorageBootcamp.swift
@@ -0,0 +1,40 @@
+//
+//  AppStorageBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by 고혜지 on 2023/07/19.
+//
+
+import SwiftUI
+
+struct AppStorageBootcamp: View {
+    
+//    @State var currentUserName: String?
+    @AppStorage("name") var currentUserName: String?
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text(currentUserName ?? "Add Name Here")
+            
+            if let name = currentUserName {
+                Text(name)
+            }
+            
+            Button("Save".uppercased()) {
+                let name = "Emily"
+                currentUserName = name
+//                UserDefaults.standard.set(name, forKey: "name")
+                
+            }
+        }
+//        .onAppear {
+//            currentUserName = UserDefaults.standard.string(forKey: "name")
+//        }
+    }
+}
+
+struct AppStorageBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AppStorageBootcamp()
+    }
+}
