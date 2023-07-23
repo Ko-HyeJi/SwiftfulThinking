397e6ad02b82a321a68453e88bda038ce5fbe0f0
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-07-23 18:19:39 +0900

    TextSelection

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,21 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by 고혜지 on 2023/07/23.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+struct TextSelectionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionBootcamp()
+    }
+}
