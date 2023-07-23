10939d1540178375977788be8785aaf5bfc87ac8
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-07-23 19:04:52 +0900

    ButtonStyles

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ButtonStylesBootcamp.swift
@@ -0,0 +1,68 @@
+//
+//  ButtonStylesBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by 고혜지 on 2023/07/23.
+//
+
+import SwiftUI
+
+struct ButtonStylesBootcamp: View {
+    var body: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Text("Button Title")
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 20))
+            .controlSize(.large)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+//            .buttonStyle(.plain)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+//            .buttonStyle(.bordered)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.regular)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.small)
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+//            .buttonStyle(.borderless)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.mini)
+        }
+        .padding()
+    }
+}
+
+struct ButtonStylesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStylesBootcamp()
+    }
+}
+
