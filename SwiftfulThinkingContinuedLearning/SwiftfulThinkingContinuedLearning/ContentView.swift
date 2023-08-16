2f18b258d449f2cf4cefa0bf37fa0b24c49862cb
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-08-16 15:51:56 +0900

    init project

---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by 고혜지 on 2023/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningApp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by 고혜지 on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
