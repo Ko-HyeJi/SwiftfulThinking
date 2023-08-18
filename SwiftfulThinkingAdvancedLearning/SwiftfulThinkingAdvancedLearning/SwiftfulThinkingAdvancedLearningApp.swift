6e3f512904be86f3b5e24a6669fc7f27ad12ccd6
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-08-18 15:19:28 +0900

    create new project

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by 고혜지 on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
