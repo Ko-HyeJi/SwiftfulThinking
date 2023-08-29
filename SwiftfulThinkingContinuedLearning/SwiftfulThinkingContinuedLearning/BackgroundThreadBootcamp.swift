d0b99a302c55e0e7ee9298e828471f753c48bc89
Ko-HyeJi <hy2ji.k5@gmail.com>
2023-08-29 17:45:52 +0900

    BackgroundThread

---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/BackgroundThreadBootcamp.swift
@@ -0,0 +1,67 @@
+//
+//  BackgroundThreadBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by 고혜지 on 2023/08/29.
+//
+
+import SwiftUI
+
+class BackgroundThreadViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    func fetchData() {
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            print("CHECK 1: \(Thread.isMainThread)")
+            print("CHECK 1: \(Thread.current)")
+            
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                print("CHECK 1: \(Thread.isMainThread)")
+                print("CHECK 1: \(Thread.current)")
+            }
+        }
+    }
+    
+    private func downloadData() -> [String] {
+        var data: [String] = []
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+    
+}
+
+struct BackgroundThreadBootcamp: View {
+    
+    @StateObject var vm = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("LOAD DATA")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
+                    }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+            }
+        }
+    }
+}
+
+struct BackgroundThreadBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundThreadBootcamp()
+    }
+}
